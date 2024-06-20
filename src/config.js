bcb249f85bfc4ec489141f2c1adf0a9736226b92
Royce Calvin <royce20002006@yahoo.com>
2024-06-19 23:05:19 -0500

    try to add to git

--- src/config.js
+++ src/config.js
@@ -111,4 +111,4 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
+export const formspreeUrl = "https://formspree.io/f/meojjjyv";
